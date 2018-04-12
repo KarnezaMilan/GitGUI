523d171ba8def28d75272b7b74715700b029eb80
My Name <milan.karneza@gmail.com>
2018-04-12 14:45:54 +0200

    Style

--- GIT_WPF/WpfApplication/UserControls/CenterArea.xaml.cs
+++ GIT_WPF/WpfApplication/UserControls/CenterArea.xaml.cs
@@ -13,14 +13,14 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace WpfApplication
+namespace WpfApplication.UserControls
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    /// Interaction logic for CenterArea.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class CenterArea : UserControl
     {
-        public MainWindow()
+        public CenterArea()
         {
             InitializeComponent();
         }
